36a2cf5ea97f2c8353f2c6b9fbc5c855916c1e3b
everythingisimportant <minhnhatt716@gmail.com>
2018-05-23 22:23:53 +0700

    This is my new project

---
+++ 1.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function(){
+	$("input").focus(function(){
+		$(this).css("background-color","#cccccc");
+	});
+
+	$("input").blur(function(){
+		$(this).css("background-color","#ffffff");
+	});
+
+	$("button").click(function(){
+		$("button").css("background-color","red");
+	});
+
+    $("p").on({
+    	mouseenter: function(){
+    		$(this).css("background-color", "blue")
+    	},
+    	mouseleave: function(){
+    		$(this).css("background-color", "orange")
+    	},
+    	click: function(){
+    		$(this).css("background-color", "red")
+    	}
+    });
+
+    $("#2").click(function(){
+    	$("div").animate({left: '500px'},2000).animate({fontSize:'3em'},1000);
+    });
+
+	$("#3").click(function(){
+	    $("div").stop();
+	})
+
+});
